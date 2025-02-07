fb88a93edff1260f2b6c2fbd2f39da61a10e9ddc
rbamboofield <187759144+rbamboofield@users.noreply.github.com>
2025-02-07 14:56:34 +0100

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringIn) {
+    const arrayStringIn = stringIn.split("");
+    const arrayStringOut = [];
+    for (let i = 0; i < stringIn.length; i++)  arrayStringOut.push(arrayStringIn.pop());
+    return arrayStringOut.join("")
 };
 
 // Do not edit below this line

c0169b95b30f47cc4f4e4beb512e6ec19e0dec98
rbamboofield <187759144+rbamboofield@users.noreply.github.com>
2025-02-07 14:44:44 +0100

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringIn, n) {
+    let stringOut = ""
+    if (n >= 0) {
+        for (let i = 0; i < n; i++) {
+            stringOut += stringIn;
+        }
+    } else {
+        return 'ERROR'
+    }
+    return stringOut
 };
 
 // Do not edit below this line

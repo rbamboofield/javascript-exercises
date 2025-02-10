d6985632c11c69369fe873f97214f18e9b9a508f
rbamboofield <187759144+rbamboofield@users.noreply.github.com>
2025-02-10 15:10:53 +0100

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayIn) {
+    var indexes = [], i, j;
+    const arrayOut = arrayIn;
+    for (j = 0; j < arguments.length - 1; j++) {
+        for (i = 0; i < arrayOut.length; i++)
+            if (arrayOut[i] === arguments[j + 1])
+                indexes.push(i);
+    }
+    indexes.sort()
+    for (i = indexes.length -1; i >= 0; i--)
+        arrayOut.splice(indexes[i], 1);
+    return arrayOut
 };
 
 // Do not edit below this line
